a85ccc0c9e30a34f59979a346193c81f874c20c9
Andrew <alo42@georgetown.edu>
2021-10-01 13:21:46 +0200

    initial commit

---
+++ hardhat.config.js
@@ -0,0 +1,23 @@
+require("@nomiclabs/hardhat-waffle");
+require('custom-env').env(process.env.DEPLOY_ENV);
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+module.exports = {
+  solidity: "0.8.4",
+  networks: {
+    rinkeby: {
+      url: process.env.PROVIDER_URL,
+      accounts: [process.env.WALLET_KEY],
+    },
+    localhost: {
+      url: process.env.PROVIDER_URL,
+    },
+  },
+};
